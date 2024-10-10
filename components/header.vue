5e3395156a96d590f3504597c504efa16120c2e7
v.haritonenko <vadimka.kharitonenko@gmail.com>
2024-10-11 05:08:31 +0700

    Define pages, global styles, default layout, title, page transition.

---
+++ components/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <NuxtLink to="/">Index</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/contact">Contact</NuxtLink>
+    <NuxtLink to="/pricing">Pricing</NuxtLink>
+    <NuxtLink to="/blog">Blog</NuxtLink>
+    <NuxtLink to="/blog/1">Detail Blog</NuxtLink>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: 'Coca',
+})
+</script>
+
+<template>
+  <div>
+    <Header />
+    <main>
+      <slot />
+    </main>
+    <footer>footer</footer>
+  </div>
+</template>
