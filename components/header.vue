ffef8d9ac21bda2db8c70ce4ae353a01cb0ae113
v.haritonenko <vadimka.kharitonenko@gmail.com>
2024-10-11 16:22:26 +0700

    Header, colors, font.

--- components/header.vue
+++ components/header.vue
@@ -1,10 +1,35 @@
 <template>
-  <header>
-    <NuxtLink to="/">Index</NuxtLink>
-    <NuxtLink to="/about">About</NuxtLink>
-    <NuxtLink to="/contact">Contact</NuxtLink>
-    <NuxtLink to="/pricing">Pricing</NuxtLink>
-    <NuxtLink to="/blog">Blog</NuxtLink>
-    <NuxtLink to="/blog/1">Detail Blog</NuxtLink>
+  <header class="header container">
+    <NuxtLink to="/">
+      <img src="/logo.svg" alt="Logo">
+    </NuxtLink>
+    <ul class="header__menu size-16">
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+      <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+      <li><NuxtLink to="/pricing">Pricing</NuxtLink></li>
+      <li class="contact w-semibold"><NuxtLink to="/contact">Contact Us -></NuxtLink></li>
+    </ul>
   </header>
 </template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 26px 0 22px;
+}
+
+.header__menu {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+}
+
+.contact {
+  margin-left: 2.25rem;
+  line-height: 2rem;
+  border-bottom: .0625rem solid var(--neutral-100);
+}
+</style>
